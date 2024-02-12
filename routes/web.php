6b25cc073588269fb93f752c6a2736d871e500ab
Zariy7 <nazariy.hereha@gmail.com>
2024-02-12 08:10:29 +0100

    welcome with data transfer

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    $data = [
+        'welcome_msg'   =>  'Hello, World!',
+    ];
+    return view('home', $data);
 });
